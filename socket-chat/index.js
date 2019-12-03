34b64abe2bb164b2d781caf6d785863785c4b47b
yde <philip.ydehed@gmail.com>
2019-12-03 18:38:43 +0100

    removed some stuff

--- socket-chat/index.js
+++ socket-chat/index.js
@@ -9,9 +9,7 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/whaddap', function (req, res) {
-    res.send("whatsupp");
-});
+
 
 io.on('connection', function (socket) {
     socket.on('new-user', name => {
@@ -28,18 +26,6 @@ io.on('connection', function (socket) {
     console.log('a user connected');
 });
 
-io.on('connection', function (socket) {
-    console.log('a user connected');
-    socket.on('disconnect', function () {
-        console.log('user disconnected');
-    });
-});
-
-io.on('connection', function (socket) {
-    socket.on('chat message', function (msg) {
-        io.emit('chat message', msg);
-    });
-});
 
 http.listen(port, function () {
     console.log('listening on port 4000');
