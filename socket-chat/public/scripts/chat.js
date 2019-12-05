1fc06d66a7cedb8baa01fa4cacf477d05142e63e
yde <philip.ydehed@gmail.com>
2019-12-05 14:35:27 +0100

    added js file and folder

---
+++ socket-chat/public/scripts/chat.js
@@ -0,0 +1,36 @@
+const socket = io('http://localhost:4000')
+const messageContainer = document.getElementById('messages')
+const messageForm = document.getElementById('send')
+const messageInput = document.getElementById('input')
+
+const name = prompt('What is your name?')
+appendMessage(`You joined as ${name}`)
+socket.emit('new-user', name)
+
+console.log(name)
+
+socket.on('chat-message', data => {
+    appendMessage(`${data.name}: ${data.message}`)
+})
+
+socket.on('user-connected', name => {
+    appendMessage(`${name} connected`)
+})
+
+socket.on('user-disconnected', name => {
+    appendMessage(`${name} disconnected`)
+})
+
+messageForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const message = messageInput.value
+    appendMessage(`${name}: ${message}`)
+    socket.emit('send', message)
+    messageInput.value = ''
+})
+
+function appendMessage(message) {
+    const messageElement = document.createElement('div')
+    messageElement.innerText = message
+    messageContainer.append(messageElement)
+}
