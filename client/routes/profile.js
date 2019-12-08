9badd9d89a488d4ca6d5a8f7cd8dd33c20654f0c
esodermark <sodermark@live.se>
2019-12-08 20:45:16 +0100

    ''

--- client/routes/profile.js
+++ client/routes/profile.js
@@ -7,50 +7,56 @@ const fetch = require('node-fetch');
 
 fetch("http://localhost:5000/api/profiles")
 .then(r =>  r.json().then(data => ({status: r.status, body: data})))
-.then(obj => console.log(obj));
+.then(function(obj){
+    console.log(obj.body.profilePicture);
 
-//Set storage engine
-const storage = multer.diskStorage({
-    destination: './public/uploads',
-    filename: function(req, file, cb){
-        cb(null, file.fieldname + '-' + Date.now() + 
-        path.extname(file.originalname));
-    }
-})
+    router.get('/profile', function(req, res, next) {
+        res.render('profile', {defaultPicture: 'uploads/defaultPicture.png'});
+      
+        next();
+      });
 
-//Init upload
-const upload = multer({
-    storage: storage,
-    limits:{fileSize: 1000000}
-}).single('profilePicture');
+    //Set storage engine
+    const storage = multer.diskStorage({
+        destination: './public/uploads',
+        filename: function(req, file, cb){
+            cb(null, file.fieldname + '-' + Date.now() + 
+            path.extname(file.originalname));
+        }
+    })
+
+    //Init upload
+    const upload = multer({
+        storage: storage,
+        limits:{fileSize: 1000000}
+    }).single('profilePicture');
 
 
-router.get('/profile', function(req, res, next) {
-  res.render('profile', {defaultPicture: 'uploads/defaultPicture.png'});
 
-  next();
-});
 
-router.post('/upload', function(req, res, next) {
-    upload(req, res, (err) => {
-        if(err){
-            res.render('profile', {
-                msg: err
-            });
-        } else{
-            if(req.file == undefined){
+    router.post('/upload', function(req, res, next) {
+        upload(req, res, (err) => {
+            if(err){
                 res.render('profile', {
-                    defaultPicture: 'uploads/defaultPicture.png'
+                    msg: err
                 });
             } else{
-                console.log(`uploads/${req.file.filename}`);
-                res.render('profile', {
-                    defaultPicture: 'uploads/defaultPicture.png',
-                    file: `uploads/${req.file.filename}`
-                });
+                if(req.file == undefined){
+                    res.render('profile', {
+                        defaultPicture: 'uploads/defaultPicture.png'
+                    });
+                } else{
+                    console.log(`uploads/${req.file.filename}`);
+                    res.render('profile', {
+                        defaultPicture: 'uploads/defaultPicture.png',
+                        file: `uploads/${req.file.filename}`
+                    });
+                }
             }
-        }
-    });
+        });
 });
+});
+
+
 
 module.exports = router;
