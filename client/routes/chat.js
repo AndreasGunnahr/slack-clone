1aa076e4f9d3bfaae2c1171603e2a78503b30c69
yde <philip.ydehed@gmail.com>
2019-12-09 13:55:40 +0100

    some style

--- client/routes/chat.js
+++ client/routes/chat.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET dashboard page. */
 router.get('/', function(req, res, next) {
     const signedInUser = req.session.activeUserInfo;
-    res.render('chat', {username: signedInUser.username});
+    res.render('chat', {username: "yde"}) //signedInUser.username});
 });
 
 module.exports = router;
