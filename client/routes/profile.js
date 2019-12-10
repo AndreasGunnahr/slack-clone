a471ec44bb93fda6938068dffddbcd4efcb514e7
esodermark <sodermark@live.se>
2019-12-10 07:36:31 +0100

    connected post req client to server

--- client/routes/profile.js
+++ client/routes/profile.js
@@ -9,78 +9,90 @@ fetch("http://localhost:5000/api/profiles")
 .then(r =>  r.json().then(data => ({status: r.status, body: data})))
 .then(function(obj){
     console.log(obj.body.profilePicture);
-
     router.get('/profile', function(req, res, next) {
-        res.render('profile', {defaultPicture: 'uploads/defaultPicture.png'});
-      
+        res.render('profile', {defaultPicture: 'uploads/defaultPicture.png', file: obj.body.profilePicture});
         next();
       });
-
-    //Set storage engine
-    const storage = multer.diskStorage({
-        destination: './public/uploads',
-        filename: function(req, file, cb){
-            cb(null, file.fieldname + '-' + Date.now() + 
-            path.extname(file.originalname));
-        }
-    })
-
-    //Init upload
-    const upload = multer({
-        storage: storage,
-        limits:{fileSize: 1000000}
-    }).single('profilePicture');
+});
 
 
+//Set storage engine
+const storage = multer.diskStorage({
+    destination: './public/uploads',
+    filename: function(req, file, cb){
+        cb(null, file.fieldname + '-' + Date.now() + 
+        path.extname(file.originalname));
+    }
+})
+
+//Init upload
+const upload = multer({
+    storage: storage,
+    limits:{fileSize: 1000000}
+}).single('profilePicture');
 
 
-    router.post('/upload', function(req, res, next) {
-        upload(req, res, (err) => {
-            if(err){
+router.post('/upload', function(req, res, next) {
+    upload(req, res, (err) => {
+        if(err){
+            console.log(err);
+            res.render('profile', {
+                msg: err,
+                defaultPicture: 'uploads/defaultPicture.png'
+            });
+        } else{
+            if(req.file == undefined){
                 res.render('profile', {
-                    msg: err
+                    defaultPicture: 'uploads/defaultPicture.png'
                 });
             } else{
-                if(req.file == undefined){
-                    res.render('profile', {
-                        defaultPicture: 'uploads/defaultPicture.png'
-                    });
-                } else{
-                    const profile = {
-                        name: 'test',
-                        src: 'uploads/test.jpg'
-                    }
-                
-                    const option = {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-                    body: JSON.stringify(profile)
-                    };
-                
-                    fetch("http://localhost:5000/login", option)
-                    .then(response => {
-                        response.json().then(function(data) {
-                            
-                            if(data.status){
-                                console.log(data);
-                            }else{
-                                console.log('err');
-                            }
-                        });
-                    });
-                    console.log(`uploads/${req.file.filename}`);
-                    res.render('profile', {
-                        defaultPicture: 'uploads/defaultPicture.png',
-                        file: `uploads/${req.file.filename}`
-                    });
+                const profile = {
+                    name: 'byid',
+                    profilePicture: `uploads/${req.file.filename}`
                 }
+                
+                const option = {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(profile)
+                };
+                
+                fetch("http://localhost:5000/api/profiles", option)
+                .then(r =>  r.text().then(data => ({status: r.status, body: data})))
+                .then(function(data){
+                });
+                console.log(`uploads/${req.file.filename}`);
+                res.render('profile', {
+                    defaultPicture: 'uploads/defaultPicture.png',
+                    file: `uploads/${req.file.filename}`
+                });
             }
+        }
+    });
+});
+
+
+router.post('/delete', function(req, res, next){
+    const profile = {
+        profilePicture: 'uploads/defaultPicture.png'
+    }
+    const option = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(profile)
+        };
+        
+        fetch("http://localhost:5000/api/profiles/delete", option)
+        .then(r =>  r.text().then(data => ({status: r.status, body: data})))
+        .then(function(data){
         });
-});
-});
-
-
+    res.render('profile', {
+        defaultPicture: 'uploads/defaultPicture.png'
+    })
+})
 
 module.exports = router;
--- server/routes/profile.js
+++ server/routes/profile.js
@@ -8,17 +8,38 @@ router.get('/profiles', function(req, res, next){
     }).catch(next);
 });
 
-router.post('/profiles', function(req, res, next){
+/*router.post('/profiles', function(req, res, next){
     Profile.create(req.body).then(function(profile){
+        res.send(profile);
+    }).catch(next);
+});*/
+
+/*router.post('/profiles', function(req, res, next){
+    Profile.findByIdAndUpdate({_id: '5dee072ce810bf0f5bc42e7b'}, req.body).then(function(profile){
+        res.send(profile);
+    }).catch(next);
+})*/
+
+router.post('/profiles', function(req, res, next){
+    Profile.findByIdAndUpdate({_id: '5dee07d5e810bf0f5bc42e7d'}, req.body).then(function(){
+    Profile.findOne({_id: '5dee07d5e810bf0f5bc42e7d'}).then(function(profile){
+        console.log(req.body.profilePicture);
+        res.send(profile);
+    });
+}).catch(next);
+})
+
+router.post('/profiles/delete', function(req, res, next){
+    Profile.findOneAndUpdate(req.body).then(function(profile){
         res.send(profile);
     }).catch(next);
 });
 
+
+
 router.put('/profiles/:id', function(req, res, next){
-    Profile.findByIdAndUpdate({_id: req.params.id}, req.body).then(function(){
-        Profile.findOne({_id: req.params.id}).then(function(profile){
-            res.render('profile', {profile: profile});
-        });
+    Profile.findOneAndUpdate(req.body).then(function(profile){
+        res.send(profile);
     }).catch(next);
 });
 
